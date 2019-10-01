6d25e2e43bd410d022305cab5ff26964afc2cac2
Torben Früchtenicht <torben@fruechtenicht.net>
2019-10-02 01:29:24 +0200

    recursion completed, replaced output profile parameter with target dir parameter

--- build_profiles.sh
+++ build_profiles.sh
@@ -1,9 +1,13 @@
 #! /usr/bin/env bash
 
+# TBD how to handle baselines for jpg vs raw files?
+# -> expect qualifier in baseline filename, e.g. "baseline_jpg" or "baseline_crw"
+# -> qualifier is added to output filenames, done?
+
 set -ue
 
 PATH=$(dirname "$0"):$PATH
-SELF=$(readlink -e "$0")
+declare -r SELF=$(readlink -e "$0")
 
 declare -r BASELINE_PP3=$(readlink -e "$1")
 if [[ -z "$BASELINE_PP3" ]]; then
@@ -11,29 +15,36 @@ if [[ -z "$BASELINE_PP3" ]]; then
     exit 1
 fi
 
+declare -r WORKING_DIR=$(dirname "$BASELINE_PP3")
+
 declare -r INPUT_PROFILE=$(readlink -e "$2")
 if [[ -z "$INPUT_PROFILE" ]]; then
     echo "Missing input profile" >&2
     exit 1
 fi
 
-declare -r OUTPUT_PROFILE=$(readlink -f "$3")
-if [[ -z "$OUTPUT_PROFILE" ]]; then
-    echo "Output profile path does not exist" >&2
-    exit 1
+if [[ -z $(readlink -e "$3") ]]; then
+    echo "Creating missing target directory $(readlink -f "$3")" >&2
+    mkdir "$(readlink -f "$3")"
 fi
+declare -r TARGET_DIR=$(readlink -e "$3")
+declare -r OUTPUT_PROFILE=$TARGET_DIR/template.pp3
 if [[ -e "$OUTPUT_PROFILE" ]]; then
-    echo "Output profile $OUTPUT_PROFILE already exists"
+    echo "Output profile $OUTPUT_PROFILE exists"
     exit 1
 fi
 
+# step 1: create the output profile for the current baseline
+echo "$INPUT_PROFILE $BASELINE_PP3 $OUTPUT_PROFILE"
 cp "$INPUT_PROFILE" "$OUTPUT_PROFILE"
 meld_baseline.sh "$BASELINE_PP3" "$OUTPUT_PROFILE"
 
-find "$(dirname "$BASELINE_PP3")" -mindepth 2 -maxdepth 2 -name 'baseline.pp3' |\
+# step 2: check if there are baselines in sub directories of WORKING_DIR. if yes, recurse to each of these directories, 
+# passing the OUTPUT_PROFILE as the next INPUT_PROFILE
+declare -r next_input_profile=$OUTPUT_PROFILE
+find "$WORKING_DIR" -mindepth 2 -maxdepth 2 -name 'baseline.pp3' |\
     while read next_baseline; do
-        $SELF "$next_baseline" "$OUTPUT_PROFILE" "$(dirname "$next_baseline")/template.pp3"
+        next_working_dir=$(dirname "$next_baseline")
+        next_target_directory="$TARGET_DIR/${next_working_dir##*/}"
+        $SELF "$next_baseline" "$next_input_profile" "$next_target_directory"
     done
-
-
-
