de45acedb8e8acb27517ef5f74fd392b60a8deb6
Torben Früchtenicht <torben@fruechtenicht.net>
2019-11-18 01:18:30 +0100

    test for archive_processed

---
+++ test/move_photos/archive_processed/test.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env bash
+
+set -e
+
+declare -r TESTDIR="$(dirname "$(readlink -e "$0")")"
+
+declare -r ARCHIVE_PROCESSED="$TESTDIR/../../../archive_processed"
+
+declare -r INPUT_DIR="$TESTDIR/input"
+declare -r OUTPUT_DIR="$TESTDIR/output"
+declare -r INCOMING_DIR="$OUTPUT_DIR/incoming"
+declare -r ARCHIVE_DIR="$OUTPUT_DIR/archive"
+set -x
+! test -e "$OUTPUT_DIR" && mkdir "$OUTPUT_DIR"
+find "$OUTPUT_DIR" -mindepth 1 -delete
+mkdir "$INCOMING_DIR"
+mkdir "$ARCHIVE_DIR"
+rsync -a "$INPUT_DIR/" "$INCOMING_DIR"
+
+"$ARCHIVE_PROCESSED" "$ARCHIVE_DIR" "$INCOMING_DIR"
+
+echo "---"
+find "$OUTPUT_DIR" -type f 
+
