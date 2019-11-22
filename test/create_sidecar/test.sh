84314c94c503c00326dcbe6b7e9fab54b6bbe14c
Torben Früchtenicht <torben@fruechtenicht.net>
2019-11-22 01:30:57 +0100

    added a runner for all tests

--- test/create_sidecar/test.sh
+++ test/create_sidecar/test.sh
@@ -17,4 +17,3 @@ echo "---"
 
 assert_correct_actual_sidecar_count 2
 assert_actual_output_matches_expected
-echo "All tests ok"
---
+++ test/run.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+declare -r TESTS_ROOT=$(dirname "$(readlink "$0")")
+
+find "$TESTS_ROOT" -type f -name 'test.sh' | while read -r test_cmd; do
+    echo 
+    echo "=========================================================" 
+    echo "Running $test_cmd"
+    echo "=========================================================" 
+    "$test_cmd"
+    if (( $? == 0 )); then
+        echo "---"
+        echo "[SUCCESS] All tests passed"
+    else 
+        echo "---"
+        echo "[FAIL] There were failures"
+    fi
+done
