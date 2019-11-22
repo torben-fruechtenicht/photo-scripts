d1dba713fb2049bdb460ce35d49cfd484ee76fb9
Torben Früchtenicht <torben@fruechtenicht.net>
2019-11-22 01:12:00 +0100

    test for set_iptc_keywords now with assertions

--- test/set_iptc_keywords/test.sh
+++ test/set_iptc_keywords/test.sh
@@ -3,6 +3,17 @@
 set -e
 . ../setup.sh
 
+assert_jpg_itpc_matches_expected() {
+    cd "$EXPECTED_DIR" && find . -type f -name '*.jpg.iptc' | while read -r expected_file; do
+        actual_file=${expected_file%.*}
+        actual_iptc=$(exiv2 -PIkt $actual_file 2> /dev/null)
+        if ! cmp -s <(echo "$actual_iptc") "$expected_file"; then
+            echo "Actual JPEG IPTC does not match expected: $(diff <(echo "$actual_iptc") "$expected_file")" 
+            exit 1
+        fi
+    done
+} 
+
 rsync -a "$INPUT_DIR/" "$OUTPUT_DIR"
 
 declare -r SET_IPTC_KEYWORDS="$PROJECT_ROOT/set_iptc_keywords"
@@ -12,4 +23,5 @@ declare -r KEYWORDS="TODO;TODO 2"
 find "$OUTPUT_DIR" -type f -name '*.ORF' | xargs "$SET_IPTC_KEYWORDS" -v "$KEYWORDS" 
 
 echo "---"
-find "$OUTPUT_DIR" -type f 
+assert_jpg_itpc_matches_expected
+assert_actual_output_matches_expected "pp3"
--- test/setup.sh
+++ test/setup.sh
@@ -5,16 +5,18 @@ declare -r INPUT_DIR="$TEST_DIR/input"
 declare -r OUTPUT_DIR="$TEST_DIR/output"
 declare -r EXPECTED_DIR="$TEST_DIR/expected"
 
-(! test -e "$OUTPUT_DIR" && mkdir "$OUTPUT_DIR") || find "$OUTPUT_DIR" -type f -delete
+(! test -e "$OUTPUT_DIR" && mkdir "$OUTPUT_DIR") || find "$OUTPUT_DIR" -type f -delete  
 
 assert_actual_output_matches_expected() {
-    cd "$EXPECTED_DIR" && find . -type f | while read -r expected_file; do
+    local -r filetype=$1
+    cd "$EXPECTED_DIR" && find . -type f ${filetype+-name '*.'$filetype} | while read -r expected_file; do
 
         if ! [[ -f "$OUTPUT_DIR/$expected_file" ]]; then
             echo "Expected file $expected_file missing"
             exit 1
         fi
 
+        # cmp does a byte-wise check
         if ! cmp -s "$EXPECTED_DIR/$expected_file" "$OUTPUT_DIR/$expected_file"; then
             echo "Actual file does not match $expected_file"
             exit 1
