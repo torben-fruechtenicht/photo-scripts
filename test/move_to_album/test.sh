c0adf7d64c93f41b2187781ebeb754ac022c55e0
Torben Früchtenicht <torben@fruechtenicht.net>
2019-11-23 01:22:26 +0100

    assertions for move_to_album

--- test/move_to_album/test.sh
+++ test/move_to_album/test.sh
@@ -1,22 +1,15 @@
 #! /usr/bin/env bash
 
 set -e
+. "$(dirname "$(readlink -e "$0")")/../setup.sh"
 
-declare -r TESTDIR="$(dirname "$(readlink -e "$0")")"
+declare -r MOVE_TO_ALBUM="$PROJECT_ROOT/move_to_album"
 
-declare -r MOVE_TO_ALBUM="$TESTDIR/../../move_to_album"
-
-declare -r INPUT_DIR="$TESTDIR/input"
-declare -r OUTPUT_DIR="$TESTDIR/output"
-
-declare -r NEW_NAME="Great-Photos"
-
-! test -e "$OUTPUT_DIR" && mkdir "$OUTPUT_DIR"
-find "$OUTPUT_DIR" -type f -delete
 rsync -a "$INPUT_DIR/" "$OUTPUT_DIR"
 
+# declare -r NEW_NAME="Great-Photos"
 find "$OUTPUT_DIR" -type f -name '*.ORF' | xargs "$MOVE_TO_ALBUM" "$OUTPUT_DIR/2019/Other-album" 
 
-echo "---"
-find "$OUTPUT_DIR" -type f 
+
+assert_created_files_match_expected
 
