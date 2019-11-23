c69bb1d7acaa5e5d3ae3361bde0baf0cef8ccded
Torben Früchtenicht <torben@fruechtenicht.net>
2019-11-23 01:16:11 +0100

    no need to group tests

---
+++ test/rename/test.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/env bash
+
+set -e
+
+declare -r TESTDIR="$(dirname "$(readlink -e "$0")")"
+
+declare -r RENAME="$TESTDIR/../../rename"
+
+declare -r ORIG_PHOTOS="$TESTDIR/orig"
+declare -r WORKING_DIR="$TESTDIR/renamed"
+
+declare -r NEW_NAME="Great-Photos"
+
+! test -e "$WORKING_DIR" && mkdir "$WORKING_DIR"
+find "$WORKING_DIR" -type f -delete
+rsync -a "$ORIG_PHOTOS/" "$WORKING_DIR"
+
+find "$WORKING_DIR" -type f -name '*.ORF' | xargs "$RENAME" "$NEW_NAME" 
+
+echo "---"
+find "$WORKING_DIR" -type f 
+
