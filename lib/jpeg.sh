bea2566274494e69fa4ae4d36aeea9259bf5388b
Torben Früchtenicht <torben@fruechtenicht.net>
2019-11-26 23:36:54 +0100

    cleanup

--- lib/jpeg.sh
+++ lib/jpeg.sh
@@ -25,22 +25,6 @@ jpeg_get_iptc() {
 	exiv2 -PIt -K "Iptc.Application2.$key" "$jpg_file" 2> /dev/null
 }
 
-# jpeg_set_exif() {
-
-# }
-
-# exiv_get_exif_key() {
-#     local -r jpg_file=$1
-#     local -r exif_key=$2
-#     # exiv2 is always printing this annoying but harmless error about date format, try to suppress only that one
-#     # but print others
-#     exiv2 pr -Pt -g "Exif.$exif_key" "$jpg_file" 2>1 | grep -v "Warning: Unsupported date format"
-# }
-
-# exiv_get_iptc_key() {
-#     local -r jpg_file=$1
-# }
-
 jpeg_set_iptc_keywords() {
     local -r keywords=$1
     local -r jpg_file=$2
@@ -54,18 +38,15 @@ jpeg_set_iptc_keywords() {
 		if [[ "$old_keywords" =~ .*$new_keyword.* ]]; then
 			continue
 		fi
-		# set -x
+
 		if [[ $new_keyword =~ .+[[:space:]].+ ]]; then
 			needs_keywords=
 		else 
 			unset needs_keywords
 		fi
 
-		# FIXME use add (only add? or set for first?) for setting keywords
-		# for quoting, use "-Madd Iptc.Application2.Keywords String '\"a b\"'"
-		exiv2 "-Madd Iptc.Application2.Keywords String '${needs_keywords+\"}$new_keyword${needs_keywords+\"}'" "$jpg_file" 2> /dev/null
-
-		# set +x
+		exiv2 "-Madd Iptc.Application2.Keywords String '${needs_keywords+\"}$new_keyword${needs_keywords+\"}'" \
+			"$jpg_file" 2> /dev/null
 	done
 
 	IFS=$OLD_IFS
