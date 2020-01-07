b6d423813fb5cef9e832103eb1ab3acbbb391995
Torben Früchtenicht <torben@fruechtenicht.net>
2020-01-07 01:23:50 +0100

    fixed number pattern

--- lib/metadata.sh
+++ lib/metadata.sh
@@ -2,7 +2,7 @@ declare -r TITLE_PATTERN="([^_]+)" # \1
 declare -r DATE_PATTERN="([0-9]{8})" # \2
 declare -r TIME_PATTERN="([0-9]{4})" # \3
 declare -r CAMERA_PATTERN="([a-zA-Z0-9-]+)" # \4
-declare -r NUMBER_PATTERN="([0-9]+(-[a-zA-Z0-9-]+)?)" # \5
+declare -r NUMBER_PATTERN="([0-9A-Z]+(-[a-zA-Z0-9-]+)?)" # \5
 declare -r FILE_EXT_PATTERN="([a-ZA-Z0-9]{3})" # \6
 
 declare -r PHOTO_FULLNAME_PATTERN="${TITLE_PATTERN}_${DATE_PATTERN}_${TIME_PATTERN}_${CAMERA_PATTERN}_${NUMBER_PATTERN}"
