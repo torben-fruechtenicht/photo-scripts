4503c8e7413020872a8cc7db97a0051da6d825b0
Torben Früchtenicht <torben@fruechtenicht.net>
2020-03-01 00:53:09 +0100

    value_from_stats_file has new parameter to pass in placeholder if no value exists

--- statistics/burndown_stats_lib.sh
+++ statistics/burndown_stats_lib.sh
@@ -23,28 +23,24 @@ EOF
 current_count() {
     local -r source_type=$1
     local -r stats_file=$2
-    count=$(value_from_stats_file $source_type "$stats_file")
-    if [[ $count != "-" ]]; then
-        echo $count
-    else 
-        echo "0"
-    fi
+    value_from_stats_file $source_type "$stats_file" "0"
 }
 
 previous_count() {
     local -r source_type=$1
     local -r index=$2
     local -r stats_file=$3
-    value_from_stats_file "previous_${source_type}_$index" "$stats_file"
+    value_from_stats_file "previous_${source_type}_$index" "$stats_file" "-"
 }
 
 value_from_stats_file() {
     local -r key=$1
     local -r stats_file=$2
+    local -r no_value_placeholder=$3
     local -r value=$(grep "$key=" "$stats_file" | cut -d '=' -f 2)
     if [[ -n $value ]]; then
         echo $value
     else
-        echo "-"
+        echo ${no_value_placeholder--}
     fi
 }
