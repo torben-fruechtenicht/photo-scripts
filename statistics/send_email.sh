405c05661f272d6677712334d1493dc4b861542a
Torben Früchtenicht <torben@fruechtenicht.net>
2020-03-04 00:05:10 +0100

    mail body is optional

--- statistics/send_email.sh
+++ statistics/send_email.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-set -eu
+set -e
 
 # set LANG because cron will not have a suitable value for LANG
 export LANG=en_US.UTF-8
@@ -18,12 +18,14 @@ if [[ -z $SUBJECT ]]; then
 fi
 
 declare -r MAIL_BODY=$(readlink -e "$3")
-if ! [[ -e $MAIL_BODY ]]; then
-	echo "[ERROR] mail body file $MAIL_BODY does not exist" >&2
-	exit 1
-fi
+
+set -u
 
 if [[ -n $MAIL_BODY ]]; then
+	if ! [[ -e $MAIL_BODY ]]; then
+		echo "[ERROR] mail body file $MAIL_BODY does not exist" >&2
+		exit 1
+	fi
 	cat "$MAIL_BODY" | mail -a 'MIME-Version: 1.0' -a 'Content-Type: text/text; charset=utf-8' -s "$SUBJECT" "$MAIL_RECIPIENT"
 else 
 	cat - | mail -a 'MIME-Version: 1.0' -a 'Content-Type: text/text; charset=utf-8' -s "$SUBJECT" "$MAIL_RECIPIENT"
