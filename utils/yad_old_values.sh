c34e70e91c1d6abffb547cee68cb2cb55e9c24c3
Torben Früchtenicht <torben@fruechtenicht.net>
2020-05-23 00:19:32 +0200

    adapt selector for selector_filters

--- utils/yad_old_values.sh
+++ utils/yad_old_values.sh
@@ -16,6 +16,7 @@ get_old_values_file() {
 }
 
 declare -r MONTHS_INCL_EMPTY_VALUE="!Jan!Feb!Mar!Apr!May!June!July!Aug!Sep!Oct!Nov!Dec"
+declare -r DAYS_OF_MONTH_INCL_EMPTY_VALUE="!01!02!03!04!05!06!07!08!09!10!11!12!13!14!15!16!17!18!19!20!21!22!23!24!25!26!27!28!29!30!31"
 
 old_values_or_default() {
     local -r old_values_file=$1
