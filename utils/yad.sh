93aaefd8047c9ff10d86826e577b1a62c31dbead
Torben Früchtenicht <torben@fruechtenicht.net>
2020-05-31 02:15:47 +0200

    rename uses new function run_yad_selector_result_action_dialog to render standard dialog for viewing matches and starting actual action

--- utils/yad.sh
+++ utils/yad.sh
@@ -15,6 +15,39 @@ run_yad() {
     yad $yad_options --title="$title" --text="$text" "$@"
 }
 
+run_yad_selector_result_action_dialog() {
+    local -r title=$1
+    set -x
+    local -r selected_photos=$2
+    set +x
+    local -r search_dir=$3
+    local -r action_text=$4
+    shift 4
+
+    local -r selected_photos_count=$(echo "$photos" | wc -l)
+    local -r selected_photos_list=$(__render_selected_photos_list "$(__remove_searchdir_from_photos_list "$selected_photos" "$search_dir")")
+    local -r text="Selected $selected_photos_count photo(s) from $search_dir:\n\n$selected_photos_list\n$action_text"
+
+    run_yad "$title" "$text" "$@"
+}
+
+__remove_searchdir_from_photos_list() {
+    local -r list=$1
+    local -r search_dir=$2
+
+    echo "$list" | while read -r photo; do 
+        echo ${photo#$search_dir/}
+    done
+}
+
+__render_selected_photos_list() {
+    local -r photos=$1
+    # TODO if more than N in list, cut list after N entries (and maybe add a button to repeat the search)
+    # TODO spreach across 2 columns if more than M entries in list
+    #       https://unix.stackexchange.com/a/59292
+    echo "$photos"
+}
+
 
 __lookup_memorized_value() {
     local -r saved_values_file=$1
