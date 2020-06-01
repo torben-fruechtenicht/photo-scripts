0d0f3708588150efab1bdab84eecc409b6d583ff
Torben Früchtenicht <torben@fruechtenicht.net>
2020-06-02 01:39:26 +0200

    adapted selector to source includes from apps/lib

---
+++ apps/lib/find_globs_factory.sh
@@ -0,0 +1,100 @@
+#! /usr/bin/env bash
+
+# TODO rename to "find_globs.sh" after month_glob_from_shortname has been removed
+
+year_glob() {
+    local -r year=${1+$1}
+    if [[ -z $year ]]; then
+        echo "????"
+    else 
+        echo "$year"
+    fi
+}
+
+month_glob() {
+    local -r month=${1+$1}
+    if [[ -z $month ]]; then
+        echo "??"
+    else 
+        echo "$month"
+    fi
+}
+
+dayofmonth_glob() {
+    local -r dayofmonth=${1+$1}
+    if [[ $dayofmonth =~ ^[0-9]{2}$ ]] && (( 1 <= $dayofmonth )) && (( $dayofmonth <= 31  )); then
+        echo "$dayofmonth"
+    else 
+        echo "??"
+    fi
+}
+
+album_glob() {
+    local -r album=${1+$1}
+    if [[ -z $album ]]; then
+        echo "*"
+    else 
+        echo "$album"
+    fi
+}
+
+month_glob() {
+    local -r month=${1+$1}
+    if [[ -z $month ]]; then
+        echo "??"
+    else 
+        echo "$month"
+    fi
+}
+
+date_path_glob() {
+    local -r year=${1+$1}
+    local -r month=${2+$2}
+    local -r dayofmonth=${3+$3}
+    echo "$(year_glob $year)-$(month_glob "$month")-$(dayofmonth_glob $dayofmonth)"
+}
+
+date_filename_glob() {
+    local -r year=${1+$1}
+    local -r month=${2+$2}
+    local -r dayofmonth=${3+$3}
+    date_path_glob $year $month $dayofmonth | tr --delete '-'
+}
+
+title_glob() {
+    local -r title=${1+$1}
+    if [[ -z $title ]]; then
+        echo "*"
+    else 
+        echo "$title"
+    fi
+}
+
+timeofday_glob() {
+    local -r timeofday=${1+$1}
+    if [[ -z $timeofday ]]; then
+        echo "????"
+    else 
+        echo "$timeofday"
+    fi
+}
+
+photonumber_glob() {
+    local -r number_tail=${1+$1}
+    if [[ -z $number_tail ]]; then
+        echo "*"
+    else 
+        echo "*$number_tail"
+    fi
+}
+
+filename_glob() {
+    local -r title=${1+$1}
+    local -r year=${2+$2}
+    local -r month=${3+$3}
+    local -r dayofmonth=${4+$4}
+    local -r timeofday=${5+$5}
+    local -r camera="*"
+    local -r photonumber_tail=${6+$6}
+    echo "$(title_glob "$title")_$(date_filename_glob $year $month $dayofmonth)_$(timeofday_glob "$timeofday")_*_$(photonumber_glob "$photonumber_tail")"
+}
--- apps/lib/form_values.sh
+++ apps/lib/form_values.sh
@@ -9,7 +9,41 @@ fi
 
 
 declare -r MONTHS="January!February!March!April!May!June!July!August!September!October!November!December"
+
+month_value_from_name() {
+    local -r name=${1+$1}
+    case $name in 
+        Jan* )
+            echo "01";;
+        Feb* )
+            echo "02";;
+        Mar* )
+            echo "03";;
+        Apr* )
+            echo "04";;
+        May )
+            echo "05";;
+        Jun* )
+            echo "06";;
+        Jul* )
+            echo "07";;
+        Aug* )
+            echo "08";;
+        Sep* )
+            echo "09";;
+        Oct* )
+            echo "10";;
+        Nov* )
+            echo "11";;
+        Dec* )
+            echo "12";;    
+        * )
+            echo;;       
+    esac
+}
+
 declare -r DAYS_OF_MONTH="01!02!03!04!05!06!07!08!09!10!11!12!13!14!15!16!17!18!19!20!21!22!23!24!25!26!27!28!29!30!31"
+
 
 __lookup_memorized_value() {
     local -r saved_values_file=$1
