4c25643c1ad99f8c29a2d9221f93f6e88c3a84b5
Torben Früchtenicht <torben@fruechtenicht.net>
2020-06-02 14:56:54 +0200

    line breaks, again

--- apps/lib/yad.sh
+++ apps/lib/yad.sh
@@ -2,12 +2,12 @@
 
 run_yad() {
     local -r title=$1
-    # "\r" adds a linebreak (text in yad is rendered with pango)
+    # "\r" adds a linebreak (text in yad is rendered with pango) - but only if we use "echo -e
     local -r text="$2\r"
     shift 2
 
     local -r yad_options="--borders=10 --fixed --center"
-    yad $yad_options --title="$title" --text="$text" "$@"
+    yad $yad_options --title="$title" --text="$(echo -e "$text")" "$@"
 }
 
 run_yad_selector_result_action_dialog() {
@@ -20,8 +20,8 @@ run_yad_selector_result_action_dialog() {
     local -r selected_photos_count=$(echo "$selected_photos" | wc -l)
     local -r selected_photos_list=$(__render_selected_photos_list \
         "$(__remove_searchdir_from_photos_list "$selected_photos" "$search_dir")")
-    # "\r" adds a linebreak (text in yad is rendered with pango)
-    local -r text="Selected $selected_photos_count photo(s) from $search_dir:\r$selected_photos_list\r$action_text"
+    # "\r" adds a linebreak (text in yad is rendered with pango) - but only if we use "echo -e
+    local -r text="$(echo -e "Selected $selected_photos_count photo(s) from $search_dir:\r\r$selected_photos_list\r$action_text")"
 
     run_yad "$title" "$text" "$@"
 }
