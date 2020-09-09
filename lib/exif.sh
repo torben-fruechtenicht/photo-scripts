da16c53b28ebd6c519e552b48d738ff3ac5a2056
Torben Früchtenicht <torben@fruechtenicht.net>
2020-09-10 00:45:31 +0200

    renamed jpeg_exif.sh to just exif.sh, is not only for jpegs

--- lib/exif.sh
+++ lib/exif.sh
@@ -1,11 +1,6 @@
-__get_filetype() {
-    local -r photofile=$1
-	echo ${photofile##*.} | tr '[:upper:]' '[:lower:]'
-}
-
 lookup_camera_from_exif() {
 	local -r photofile=$1
-	local -r file_type=$(__get_filetype "$photofile")
+	local -r file_type=$(tr '[:upper:]' '[:lower:]' <<<${photofile##*.})
 
 	case $file_type in 
 		raw )
@@ -18,7 +13,7 @@ lookup_camera_from_exif() {
 
 lookup_lens_from_exif() {
 	local -r photofile=$1
-	local -r file_type=$(__get_filetype "$photofile")
+	local -r file_type=$(tr '[:upper:]' '[:lower:]' <<<${photofile##*.})
 
 	case $file_type in 
 		raw|crw|cr2 )
@@ -34,7 +29,7 @@ lookup_lens_from_exif() {
 
 lookup_manufacturer_from_exif() {
 	local -r photofile=$1
-	local -r file_type=$(__get_filetype "$photofile")
+	local -r file_type=$(tr '[:upper:]' '[:lower:]' <<<${photofile##*.})
 
 	case $file_type in 
 		raw )
@@ -42,4 +37,16 @@ lookup_manufacturer_from_exif() {
 		*) 
 			exiv2 pr -Pt -g "Exif.Image.Make" "$photofile" 2> /dev/null | xargs;;
 	esac
+}
+
+lookup_image_width_from_exif() {
+	local -r photofile=$1
+	# yes, there should be a way to get the correct dimension from exif tags (instead of brute forcing
+	# exiv2 default output). but I don't know how to do it. some cameras give strange numbers...
+	exiv2 "$photofile" | grep "Image size" | sed -r 's/Image size\s+:\s+([0-9]+)\sx\s([0-9]+)/\1/'
+}
+
+lookup_image_height_from_exif() {
+	local -r photofile=$1
+	exiv2 "$photofile" | grep "Image size" | sed -r 's/Image size\s+:\s+([0-9]+)\sx\s([0-9]+)/\2/'
 }
