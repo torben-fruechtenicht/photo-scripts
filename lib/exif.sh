6afa30846094849e5d2a24ff9ad70289a1cc1f7e
Torben Früchtenicht <torben@fruechtenicht.net>
2023-01-30 23:35:57 +0100

    Current exiv2 version no  longer want "pr" together with "-P"

--- lib/exif.sh
+++ lib/exif.sh
@@ -4,9 +4,9 @@ lookup_camera_from_exif() {
 
 	case $file_type in 
 		raw )
-			exiv2 pr -Pt -g "Exif.PanasonicRaw.Model" "$photofile" 2> /dev/null;;
+			exiv2 -Pt -g "Exif.PanasonicRaw.Model" "$photofile" 2> /dev/null;;
 		*) 
-			local camera=$(exiv2 pr -Pt -g 'Exif.Image.Model' "$photofile" 2> /dev/null)
+			local camera=$(exiv2 -Pt -g 'Exif.Image.Model' "$photofile" 2> /dev/null)
 			echo "${camera//[[:space:]]/}";;
 	esac
 }
@@ -19,9 +19,9 @@ lookup_lens_from_exif() {
 		raw|crw|cr2 )
 			;; # do nothing, the only cameras we have with these types are compact cameras
 		orf)
-			exiv2 pr -Pt -g "Exif.OlympusEq.LensModel" "$photofile" 2> /dev/null;;
+			exiv2 -Pt -g "Exif.OlympusEq.LensModel" "$photofile" 2> /dev/null;;
 		jpg )
-			exiv2 pr -Pt -g  "Exif.Photo.LensModel" "$photofile" 2> /dev/null;;
+			exiv2 -Pt -g  "Exif.Photo.LensModel" "$photofile" 2> /dev/null;;
 		*) 
 			echo "[WARN] Unknown filetype $file_type of $photofile, cannot lookup lens info" >&2;;
 	esac
@@ -33,9 +33,9 @@ lookup_manufacturer_from_exif() {
 
 	case $file_type in 
 		raw )
-			exiv2 pr -Pt -g "Exif.PanasonicRaw.Make" "$photofile" 2> /dev/null | xargs;;
+			exiv2 -Pt -g "Exif.PanasonicRaw.Make" "$photofile" 2> /dev/null | xargs;;
 		*) 
-			exiv2 pr -Pt -g "Exif.Image.Make" "$photofile" 2> /dev/null | xargs;;
+			exiv2 -Pt -g "Exif.Image.Make" "$photofile" 2> /dev/null | xargs;;
 	esac
 }
 
