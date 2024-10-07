42dda99656a2d5949d46cc99910d9b292f01f998
Torben Früchtenicht <torben@fruechtenicht.net>
2024-10-07 23:26:24 +0200

    move is now using the new add/remove_keywords commands, with better removal of old album titles

---
+++ lib/directories.sh
@@ -0,0 +1,28 @@
+# All paths passed to functions must be at least relative to the collection root. Absolute paths are even better.
+
+# "leaf" is the directory where the original photo files are located. There can be directories below (like "converted")
+# but that is no longer part of the collection directory layout. Therefore, it is the starting point if we need
+# to locate specific collection directories in the hierarchy above.
+__directories_locate_collection_leaf_path() {
+    local direct_parent=${1%/*}
+    if [[ $direct_parent =~ ".*/converted" ]]; then 
+        echo ${direct_parent%/*}
+    else
+        echo "$direct_parent"
+    fi
+}
+
+directories_lookup_album_from_file() {
+    local leaf_path=$(__directories_locate_collection_leaf_path "$1")
+    local album_path=${leaf_path%/????-??-??}
+    echo "${album_path##*/}"
+}
+
+directories_collect_albums_from_files() {
+    for file in $@; do 
+        echo $(directories_lookup_album_from_file "$file")
+    done | sort -u | paste -s -d ";"
+}
+
+# TODO more candidates for functions here:
+# - create_photofile_path_from $year $day $album
